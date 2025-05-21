71cc8cea7ef6d5f7037cbba5e25e7522727a85e3
miguelToscano <miguetoscano288@gmail.com>
2025-05-21 16:18:37 -0300

    Added Dockerfile and docker-compose files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { HealthModule } from './health/health.module';
+import { UsersModule } from './users/users.module';
+import { PortfoliosModule } from './portfolios/portfolios.module';
 
 @Module({
-  imports: [],
+  imports: [HealthModule, UsersModule, PortfoliosModule],
   controllers: [AppController],
   providers: [AppService],
 })
